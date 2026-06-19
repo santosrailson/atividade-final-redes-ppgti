cd8e65127e1cd0d39b8239c22eca77a2512eaa48
Railson Santos <railsonsantospb@gmail.com>
2026-06-19 14:36:09 +0000

    Remove All

--- util/executar_topologia.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-RAIZ_PROJETO="$(cd "$(dirname "$0")/.." && pwd)"
-export PYTHONPATH="${RAIZ_PROJETO}/mininet:${PYTHONPATH}"
-
-python3 "${RAIZ_PROJETO}/mininet/topologia.py" --cli "$@"
